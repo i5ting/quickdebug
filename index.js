7c9c5c432d9878d6fc226128a07ce7fca92747d2
i5ting <i5ting@126.com>
2021-02-24 00:12:06 +0800

    document, please ignore

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 module.exports = function quickdebug(name) {
     var key = name ? name : require('get_closest_package_json').name
-    console.log(key)
-    
+
     return debug = require("colondebug")(key);
 }
